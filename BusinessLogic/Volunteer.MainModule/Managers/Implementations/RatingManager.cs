63635a0a2a9fef98fc49d1a1da043faca8dbb2b2
Karabok <maxkarab@yandex.ru>
2019-09-27 22:32:07 +0400

    Добавил логин в Юзера. Добавил реализацию Рейтинга и Менеджер рейтинга. Добавил реализацию Юзера менеджера

---
+++ BusinessLogic/Volunteer.MainModule/Managers/Implementations/RatingManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Volunteer.MainModule.Managers.Implementations
+{
+    using System.Collections.Generic;
+    using Volunteer.BLModels.Entities;
+    using Volunteer.MainModule.Managers.DataManagers.Filters;
+    using DataManagers;
+
+    public class RatingManager : ISimpleManager<Rating>
+    {
+        private readonly IDataManager<Rating> dataManager;
+
+        public RatingManager(IDataManager<Rating> dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IEnumerable<Rating> Find(Filter filter = null)
+        {
+            if (filter == null)
+            {
+                this.dataManager.GetAll();
+            }
+
+            return dataManager.GetAll(a => filter.Check<Rating>(a));
+        }
+
+        public bool Save(Rating entity)
+        {
+            return this.dataManager.Save(entity);
+        }
+    }
+}
