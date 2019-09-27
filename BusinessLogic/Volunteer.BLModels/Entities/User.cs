d1eb6ad0c6846502169a28637766def397b4bd3d
Karabok <maxkarab@yandex.ru>
2019-09-27 20:49:54 +0400

    Добавил сущность юзера, перенес проект сущностей в папку

--- BusinessLogic/Volunteer.BLModels/Entities/User.cs
+++ BusinessLogic/Volunteer.BLModels/Entities/User.cs
@@ -2,9 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
+    using Interfaces;
 
-    public class User
+    public class User : IEntity, IRatingable
     {
+        public Guid Uid { get; set; }
+        public string FullName { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<ActivitiesUsers> ActivitiesUsers { get; set; }
+        public IRating Rating { get; set; }
     }
 }
