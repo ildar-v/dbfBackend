df2bf1b40829ae13ca2b80f8d063d1a6c689bfe3
RomanChernyshev <aartman@rambler.ru>
2019-09-27 21:11:36 +0300

    Добавлен слой бизнес логики

---
+++ Volunteer.MainModule/Volunteer.MainModule/Managers/DataManagers/IDataManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Volunteer.MainModule.Managers.DataManagers
+{
+    public interface IDataManager<T>
+    {
+        IEnumerable<T> GetAll(Predicate<T> filterPredicate = null);
+
+        bool Save(T enitity);
+    }
+}
